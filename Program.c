e110506451b52c3a39b13239e519698cbc6fbc28
habibicodz <habibicodz@gmail.com>
2024-05-20 17:01:14 +0430

    initial commit

--- Program.c
+++ Program.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void printCurrentState(char(*)[3]);
 void reset(char(*)[3]);
@@ -6,8 +7,18 @@ void reset(char(*)[3]);
 int main () {
 	char tickTackToe[3][3];
 	reset(tickTackToe);
-	printCurrentState(tickTackToe);
-}
+
+	bool isEnded = false;
+	bool turnX = true;
+
+	while (!isEnded) {
+		// Print the state first
+		printCurrentState(tickTackToe);
+		int input = '';
+		scanf("%d", &input);
+
+	}
+} 
 
 void reset(char game[3][3]) {
 	int row;
@@ -33,8 +44,13 @@ void printCurrentState(char game[3][3]) {
 
 	for (row = 0; row < 3; row++) {
 		for (column = 0; column < 3; column++) {
-			int order = column + (row * columns);
-			printf("%d ", order+1);
+			char choice = game[row][column];
+			bool isEmpty = choice == ' ';
+
+			if (choice == ' ') {
+				int order = column + (row * columns);
+				printf("%d ", order+1);
+			}
 		}
 		printf("\n", "]");
 	}
