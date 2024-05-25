02a4df0e1ddb727fa30f305988693cebb604e6fc
habibicodz <habibicodz@gmail.com>
2024-05-25 07:32:53 +0430

    initial commit

--- Program.c
+++ Program.c
@@ -1,57 +1,84 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void printCurrentState(char(*)[3]);
-void reset(char(*)[3]);
+#define N 3
+
+void printCurrentState(char(*)[N]);
+void reset(char(*)[N]);
+void convertTo2D(int index, int *row, int *column);
+void getCharAtIndex(int index, char(*)[N]);
 
 int main () {
-	char tickTackToe[3][3];
+	char tickTackToe[N][N];
 	reset(tickTackToe);
 
 	bool isEnded = false;
 	bool turnX = true;
 
-	while (!isEnded) {
-		// Print the state first
-		printCurrentState(tickTackToe);
-		int input = '';
-		scanf("%d", &input);
+	bool isFirst = true;
+	char firstChar = 'x';
+	char secondChar = 'y';
 
+	while (!isEnded) {
+		printCurrentState(tickTackToe);
+
+		char currentPlayer = ((isFirst) ? firstChar : secondChar);
+		printf("%c: ", currentPlayer);
+
+		int positionChoosed = 0;
+		scanf("%d", &positionChoosed);
+
+		getCharAtIndex(positionChoosed, tickTackToe);
+
+
+		printf("position choosed: %d\n", positionChoosed);
 	}
 } 
 
-void reset(char game[3][3]) {
+void reset(char game[N][N]) {
 	int row;
 	int column;
 
-	int rows = 3;
-	int columns = 3;
+	int rows = N;
+	int columns = N;
 
-	for (row = 0; row < 3; row++) {
-		for (column = 0; column < 3; column++) {
+	for (row = 0; row < N; row++) {
+		for (column = 0; column < N; column++) {
 			game[row][column] = ' ';
 		}
 	}
 }
 
+void getCharAtIndex (int index, char game[N][N]) {
+	int row, column;
+	convertTo2D(index, &row, &column);
 
-void printCurrentState(char game[3][3]) {
+	printf("current: (%c)", game[row][column]);
+}
+
+
+void printCurrentState(char game[N][N]) {
 	int row;
 	int column;
 
-	int rows = 3;
-	int columns = 3;
+	int rows = N;
+	int columns = N;
 
-	for (row = 0; row < 3; row++) {
-		for (column = 0; column < 3; column++) {
+	for (row = 0; row < N; row++) {
+		for (column = 0; column < N; column++) {
 			char choice = game[row][column];
 			bool isEmpty = choice == ' ';
 
-			if (choice == ' ') {
-				int order = column + (row * columns);
-				printf("%d ", order+1);
+			if (isEmpty) {
+				int _1DPosition = row * columns + column;
+				printf("%d ", _1DPosition+1);
 			}
 		}
 		printf("\n", "]");
 	}
+}
+
+void convertTo2D (int index, int *row, int *column) {
+	*row = index / N;
+	*column = index % N;
 }
