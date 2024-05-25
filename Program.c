56c52cedcba1a93e4c7329136758f6ba321a915a
habibicodz <habibicodz@gmail.com>
2024-05-25 18:17:32 +0430

    initial commit

--- Program.c
+++ Program.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 #define N 3
@@ -6,7 +7,15 @@
 void printCurrentState(char(*)[N]);
 void reset(char(*)[N]);
 void convertTo2D(int index, int *row, int *column);
-void getCharAtIndex(int index, char(*)[N]);
+void convertTo1D(int row, int column, int *index);
+char getCharAtIndex(int index, char(*)[N]);
+bool fillSpace (char player, int index, char(*)[N]);
+bool checkRows(char game[N][N]);
+bool checkColumns(char game[N][N]);
+bool checkDiagonals(char game[N][N]);
+bool checkAntiDiagonals(char game[N][N]);
+bool checkWin (char game[N][N]);
+void clearScreen();
 
 int main () {
 	char tickTackToe[N][N];
@@ -19,28 +28,42 @@ int main () {
 	char firstChar = 'x';
 	char secondChar = 'y';
 
-	while (!isEnded) {
-		printCurrentState(tickTackToe);
+	clearScreen();
+	printCurrentState(tickTackToe);
 
+	while (!isEnded) {
+
+		//Get the current player
 		char currentPlayer = ((isFirst) ? firstChar : secondChar);
 		printf("%c: ", currentPlayer);
 
 		int positionChoosed = 0;
 		scanf("%d", &positionChoosed);
 
-		getCharAtIndex(positionChoosed, tickTackToe);
+		char characterAtChoosed = getCharAtIndex(positionChoosed, tickTackToe);
+		//Clear the screen first
+		clearScreen();
 
+		if (fillSpace(currentPlayer, positionChoosed - 1, tickTackToe)) {
+			if (checkWin(tickTackToe)) {
+				isEnded = true;
+				continue;
+			} else {
+				isFirst = !isFirst;
+			}
+		} else {
+			printf("%d Wrong position choosed\n", positionChoosed);
+		}
 
-		printf("position choosed: %d\n", positionChoosed);
+		//Everytime something is changed Print the game
+		printCurrentState(tickTackToe);
+		printf("%c %c %c \n", tickTackToe[0][0], tickTackToe[0][1], tickTackToe[0][2]);
 	}
 } 
 
 void reset(char game[N][N]) {
 	int row;
 	int column;
-
-	int rows = N;
-	int columns = N;
 
 	for (row = 0; row < N; row++) {
 		for (column = 0; column < N; column++) {
@@ -49,11 +72,10 @@ void reset(char game[N][N]) {
 	}
 }
 
-void getCharAtIndex (int index, char game[N][N]) {
+char getCharAtIndex (int index, char game[N][N]) {
 	int row, column;
 	convertTo2D(index, &row, &column);
-
-	printf("current: (%c)", game[row][column]);
+	return game[row][column];
 }
 
 
@@ -67,11 +89,13 @@ void printCurrentState(char game[N][N]) {
 	for (row = 0; row < N; row++) {
 		for (column = 0; column < N; column++) {
 			char choice = game[row][column];
-			bool isEmpty = choice == ' ';
+			bool isEmpty = (choice == ' ');
 
 			if (isEmpty) {
 				int _1DPosition = row * columns + column;
 				printf("%d ", _1DPosition+1);
+			} else {
+				printf("%c", choice);
 			}
 		}
 		printf("\n", "]");
@@ -81,4 +105,129 @@ void printCurrentState(char game[N][N]) {
 void convertTo2D (int index, int *row, int *column) {
 	*row = index / N;
 	*column = index % N;
+}
+
+bool fillSpace(char player, int index, char game[N][N]) {
+	int row, column;
+	convertTo2D(index, &row, &column);
+	if (game[row][column] != ' ') return false;
+	game[row][column] = player;
+	return true;
+}
+
+bool checkWin (char game[N][N]) {
+	//Check Rows
+	if (checkRows(game)) {
+		return true;
+	} else if (checkColumns(game)) {
+		return true;
+	} else if (checkDiagonals(game)) {
+		return true;
+	} else if (checkAntiDiagonals(game)) {
+		return true;
+	}
+
+	return false;
+}
+
+void clearScreen() {
+	system("cls");
+}
+
+bool checkRows(char game[N][N]) {
+	bool checkWin = false;
+
+	OUTER:for (int row = 0; row < N; row++) {
+		if (checkWin) break;
+
+		int filledTime = 0;
+		char playerToCheck = game[row][0];
+
+		for (int column = 0; column < N; column++) {
+			char currentChar = game[row][column];
+
+			if (currentChar != ' ' && game[row][column] == playerToCheck) {
+				filledTime++;
+			}
+
+			if (filledTime == N) {
+				printf("Player %c win the game\n", playerToCheck);
+				checkWin = true;
+				goto OUTER;
+			}
+		}
+	}
+
+	return checkWin;
+}
+
+bool checkDiagonals (char game[N][N]) {
+	int filledTime = false;
+	char playerToCheck = game[0][0];
+
+	for (int i = 0; i < N; i++) {
+		char c = game[i][i];
+
+		if (c != ' ' && c == playerToCheck) {
+			filledTime++;
+		}
+
+		if (filledTime == N) {
+			printf("Player %c win the game\n", playerToCheck);
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool checkAntiDiagonals (char game[N][N]) {
+	int filledTime = false;
+	char playerToCheck = game[N - 1][N - 3];
+
+	for (int i = 0; i < N; i++) {
+		char c = game[i][N - 1 - i];
+
+		if (c != ' ' && c == playerToCheck) {
+			filledTime++;
+		}
+
+		if (filledTime == N) {
+			printf("Player %c win the game\n", playerToCheck);
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool checkColumns(char game[N][N]) {
+	bool checkWin = false;
+
+	OUTER:for (int column = 0; column < N; column++) {
+		if (checkWin) break;
+
+		int filledTime = 0;
+		char playerToCheck = game[0][column];
+
+		for (int row = 0; row < N; row++) {
+			char currentChar = game[row][column];
+
+			if (currentChar != ' ' && game[row][column] == playerToCheck) {
+				filledTime++;
+			}
+
+			if (filledTime == N) {
+				printf("Player %c win the game\n", playerToCheck);
+				checkWin = true;
+				goto OUTER;
+			}
+		}
+	}
+
+	return checkWin;
+}
+
+void convertTo1D(int column, int row, int *index) {
+	*index = row * N + column;
 }
